4edef7d3397f43ae036ed944790a689aa8e1e49a
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2022-11-22 17:57:55 -0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
+function App () {
   return (
-    <div className="App">
+    <div className='App'>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,7 +19,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
