e71db120f0557208cfd229c254f6f5b7b3b91792
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2022-11-23 23:21:03 -0300

    feat: add Custom-Button to Login Page

---
+++ src/components/custom-button/custom-button.component.tsx
@@ -0,0 +1,21 @@
+/* QUANDO VOCÊ QUISER HERDAR PROPS DE UM ELEMENTO HTML USE O NOMEDOELEMENTOHTMLATTIBUTES */
+
+import React, { FunctionComponent, ButtonHTMLAttributes } from 'react'
+
+// Styles
+import { CustomButtonContainer, IconContainer } from './custom-buttom.styles'
+
+interface CustomButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
+  children: string,
+  startIcon?: React.ReactNode;/* passando um component como props */
+}
+
+const CustomButton:FunctionComponent<CustomButtonProps> = ({ children, startIcon, ...rest }) => {
+  return (
+      <CustomButtonContainer {...rest}>
+        {startIcon && <IconContainer>{startIcon}</IconContainer> }
+          {children}
+      </CustomButtonContainer>
+  )
+}
+export default CustomButton
