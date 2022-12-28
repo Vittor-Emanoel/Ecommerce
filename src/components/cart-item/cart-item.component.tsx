7ad536cd1e8f50e552cd1ed38c26975478a2e2d3
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2022-12-28 12:15:54 -0300

    feat: ensure products can be removed from cart

--- src/components/cart-item/cart-item.component.tsx
+++ src/components/cart-item/cart-item.component.tsx
@@ -1,5 +1,6 @@
-import { FunctionComponent } from 'react'
+import { FunctionComponent, useContext } from 'react'
 import { AiOutlineClose, AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai'
+import { CartContext } from '../../contexts/cart.context'
 
 // Ultilites
 import CartProduct from '../../types/cart.types'
@@ -19,6 +20,12 @@ interface CartItemProps {
 }
 
 const CartItem: FunctionComponent<CartItemProps> = ({ product }) => {
+  const { removeProductFromCart } = useContext(CartContext)
+
+  const handleRemoveClick = () => {
+    removeProductFromCart(product.id)
+  }
+
   return (
     <CartItemContainer>
         <CartItemImage imageUrl={product.imageUrl} />
@@ -33,7 +40,7 @@ const CartItem: FunctionComponent<CartItemProps> = ({ product }) => {
           <AiOutlinePlus size={20}/>
         </CartItemQuantity>
       </CartItemInfo>
-      <RemoveButton>
+      <RemoveButton onClick={handleRemoveClick}>
         <AiOutlineClose size={25} />
       </RemoveButton>
     </CartItemContainer>
--- src/contexts/cart.context.tsx
+++ src/contexts/cart.context.tsx
@@ -7,13 +7,15 @@ interface ICartContext {
   products: CartProduct[]
   toggleCart: () => void
   addProductToCart: (product: Product) => void
+  removeProductFromCart: (productId: String) => void
 }
 
 export const CartContext = createContext<ICartContext>({
   isVisible: false,
   products: [],
   toggleCart: () => {},
-  addProductToCart: () => {}
+  addProductToCart: () => {},
+  removeProductFromCart: () => {}
 })
 
 const CartContextProvider: FunctionComponent = ({ children }) => {
@@ -39,8 +41,12 @@ const CartContextProvider: FunctionComponent = ({ children }) => {
     setProducts(prevState => [...prevState, { ...product, quantity: 1 }])
   }
 
+  const removeProductFromCart = (productId: String) => {
+    setProducts(products => products.filter(product => product.id !== productId))
+  }
+
   return (
-    <CartContext.Provider value={{ isVisible, products, toggleCart, addProductToCart }}>
+    <CartContext.Provider value={{ isVisible, products, toggleCart, addProductToCart, removeProductFromCart }}>
         {children}
     </CartContext.Provider>
   )
