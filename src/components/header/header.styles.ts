8c42db1e321fa88824d932a1c7525f68f93543d9
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2022-12-27 22:23:39 -0300

    style: add responsiveness to Header component

--- src/components/header/header.styles.ts
+++ src/components/header/header.styles.ts
@@ -8,19 +8,33 @@ export const HeaderContainer = styled.div`
   justify-content: space-between;
   padding: 20px;
   color: ${Colors.text.white};
+
+  @media(max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
 `
 
 export const HeaderTitle = styled.h2`
   font-weight: bold;
   font-size: 1.5rem;
   cursor: pointer;
+  @media(max-width: 768px) {
+    margin-bottom: 1rem;
+  }
 `
 
 export const HeaderItems = styled.div`
  display: flex;
   align-items: center;
   justify-content: space-between;
-`
+
+  @media(max-width: 768px) {
+    width: 100%;
+    justify-content: space-evenly;
+  }
+  `
 
 export const HeaderItem = styled.div`
   font-weight: 600;
@@ -34,6 +48,9 @@ export const HeaderItem = styled.div`
   &:nth-child(2),
   &:nth-child(3){
     margin-right: 40px;
+    @media(max-width: 768px) {
+      margin-right: 0;
+    }
   }
 
   &:hover{
