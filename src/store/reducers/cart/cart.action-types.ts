3be6a8a7cf899e454aafeecbefd826faf4849b64
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2023-08-23 15:52:43 -0300

    feat: add types in Cart reducer

--- src/store/reducers/cart/cart.action-types.ts
+++ src/store/reducers/cart/cart.action-types.ts
@@ -1,10 +1,10 @@
 const CartActionTypes = {
-  toggleCart: 'cart/toggle',
-  addProductToCart: 'cart/addProduct',
-  removeProductFromCart: 'cart/removeProduct',
-  increaseCartProductQuantity: 'cart/increaseCartProductQuantity',
-  decreaseCartProductQuantity: 'cart/decreaseCartProductQuantity',
-  clearCartProducts: 'cart/clearCartProducts'
+  toggleCart: 'cart/toggle' as const,
+  addProductToCart: 'cart/addProduct' as const,
+  removeProductFromCart: 'cart/removeProduct' as const,
+  increaseCartProductQuantity: 'cart/increaseCartProductQuantity' as const,
+  decreaseCartProductQuantity: 'cart/decreaseCartProductQuantity' as const,
+  clearCartProducts: 'cart/clearCartProducts' as const
 }
 
 export default CartActionTypes
